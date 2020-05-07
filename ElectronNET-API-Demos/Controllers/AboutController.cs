7103118b16fae73151748e39c7dc3466bc272b2d
唐召明 <2270969436@qq.com>
2020-05-07 14:57:06 +0800

    添加项目文件。

---
+++ ElectronNET-API-Demos/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronNET_API_Demos_Zh_CN.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ElectronNET-API-Demos/Controllers/DesktopCapturerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElectronNET_API_Demos_Zh_CN.Controllers
+{
+    public class DesktopCapturerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
